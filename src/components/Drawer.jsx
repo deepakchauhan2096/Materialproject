e708c8fca3cdc0e39a455babc6976848c8625bc1
deepakchauhan2096 <deepakchauhan2096@gmail.com>
2023-06-27 00:35:03 +0530

    new

---
+++ src/components/Drawer.jsx
@@ -0,0 +1,70 @@
+import * as React from 'react';
+import Box from '@mui/material/Box';
+import Drawer from '@mui/material/Drawer';
+import Button from '@mui/material/Button';
+import List from '@mui/material/List';
+import Divider from '@mui/material/Divider';
+import ListItem from '@mui/material/ListItem';
+import ListItemButton from '@mui/material/ListItemButton';
+import ListItemIcon from '@mui/material/ListItemIcon';
+import ListItemText from '@mui/material/ListItemText';
+import InboxIcon from '@mui/icons-material/MoveToInbox';
+import MailIcon from '@mui/icons-material/Mail';
+import MenuItem from '@mui/material/MenuItem';
+import MenuIcon from '@mui/icons-material/Menu';
+import { useState } from 'react';
+import { IconButton } from '@mui/material';
+
+export default function TemporaryDrawer() {
+  
+    const [open, setOpen] =useState(false);
+
+    const handleClose = () =>{
+        setOpen(false)
+    }
+
+    const handleOpen = () => {
+        setOpen(true)
+    }
+
+
+  const Lists = () => {
+    return (<Box role="presentation">
+      <List>
+        {['All mail', 'Trash', 'Spam'].map((text, index) => (
+          <ListItem key={text} disablePadding>
+            <ListItemButton>
+              <ListItemIcon>
+                {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
+              </ListItemIcon>
+              <ListItemText primary={text} />
+            </ListItemButton>
+          </ListItem>
+        ))}
+      </List>
+    </Box>)
+    }
+
+  return (
+    <div>
+        <React.Fragment>
+            <IconButton
+              size="large"
+              aria-label="account of current user"
+              aria-controls="menu-appbar"
+              aria-haspopup="true"
+              onClick={handleOpen}
+              color="inherit"
+            >
+              <MenuIcon />
+            </IconButton>
+          <Drawer
+            open={open}
+            onClose={handleClose}
+          >
+            <Lists />
+          </Drawer>
+        </React.Fragment>
+    </div>
+  );
+}
---
+++ src/pages/home.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import AppBar from '../components/Appbar'
+import Drawer from "../components/Drawer"
+
+const home = () => {
+  return (
+    <>
+    <AppBar />
+    </>
+  )
+}
+
+export default home
